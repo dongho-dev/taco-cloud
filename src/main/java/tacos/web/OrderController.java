fc05ef02544547607f59f48839923a4d91f35f46
asdfgasdfasdfg <hdhhdh3000@naver.com>
2025-07-29 14:26:14 +0900

    feat: add POST mapping to process order submission and redirect to /

--- src/main/java/tacos/web/OrderController.java
+++ src/main/java/tacos/web/OrderController.java
@@ -3,6 +3,7 @@ package tacos.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import lombok.extern.slf4j.Slf4j;
 import tacos.Order;
@@ -16,6 +17,12 @@ public class OrderController {
 	public String orderForm(Model model) {
 		model.addAttribute("order", new Order());
 		return "orderForm";
+	}
+
+    @PostMapping
+	public String processOrder(Order order) {
+		log.info("Order submitted: " + order);
+		return "redirect:/";
 	}
 
 }
